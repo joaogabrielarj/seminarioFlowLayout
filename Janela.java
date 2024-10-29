c2143622bd8d8004c0c5d6400c1f65b57f58bec3
João Araújo <joaogabrielaraujo30@gmail.com>
2024-10-29 02:06:45 -0300

    commit eventos

---
+++ Janela.java
@@ -0,0 +1,125 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Janela extends JFrame {
+
+    private JButton bConfirm, bWant, bPlus, bSair, bOk;
+    private JLabel imagemLabel;
+
+    public Janela() {
+
+        super("Aula de Programação 2 com Emanoel Barreiros");
+
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        ImageIcon imagemOriginal = new ImageIcon("Imagem\\cartaz.png");
+        Image imagemRedimensionada = imagemOriginal.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        ImageIcon imagem = new ImageIcon(imagemRedimensionada);
+        imagemLabel = new JLabel(imagem);
+        add(imagemLabel);
+
+        bConfirm = new JButton("Confirmar Presença");
+        bWant = new JButton("Quero me Inscrever");
+        bPlus = new JButton("Saiba Mais!");
+
+        add(bConfirm);
+        add(bPlus);
+        add(bWant);
+
+        JPanel quadradosCaixinhas = new JPanel();
+        quadradosCaixinhas.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        add(quadradosCaixinhas);
+
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 550);
+        // pack();
+
+        bConfirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().removeAll();
+                setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                ImageIcon novaImagem = new ImageIcon("Imagem\\confirmed.png");
+                Image imagemNovaRedimensionada = novaImagem.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+                ImageIcon imagemCentralizada = new ImageIcon(imagemNovaRedimensionada);
+                JLabel imagemCentral = new JLabel(imagemCentralizada);
+                add(imagemCentral);
+
+                bSair = new JButton("Sair");
+                bSair.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.exit(0); // Fecha o programa
+                    }
+                });
+                add(bSair);
+
+                revalidate();
+                repaint();
+            }
+        });
+
+        bPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().removeAll();
+                setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                JLabel textoTopo = new JLabel("Este é um evento que ensina as melhores práticas de Java do mercado");
+                add(textoTopo);
+
+
+                bSair = new JButton("Sair");
+                bSair.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.exit(0); // Fecha o programa
+                    }
+                });
+                add(bSair);
+
+                revalidate();
+                repaint();
+            }
+        });
+
+        bWant.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().removeAll();
+                setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                add(new JLabel("Nome:"));
+                JTextField nomeCampo = new JTextField(15);
+                add(nomeCampo);
+
+                add(new JLabel("Sobrenome:"));
+                JTextField sobrenomeCampo = new JTextField(15);
+                add(sobrenomeCampo);
+
+                add(new JLabel("Número:"));
+                JTextField numeroCampo = new JTextField(15);
+                add(numeroCampo);
+
+                bOk = new JButton("OK");
+                bOk.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.exit(0); // Fecha o programa
+                    }
+                });
+                add(bOk);
+
+                revalidate();
+                repaint();
+            }
+        });
+    }
+}
